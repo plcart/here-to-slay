8e999610289695f9d68edd4cd0e98ae2a5556cc9
Arthur Albuquerque Ferreira <ALBUQAR@vzc.global>
2020-03-10 16:48:11 +0000

    Monster Deck to right + All standard monsters added

--- src/store/reducers/monsters.js
+++ src/store/reducers/monsters.js
@@ -144,11 +144,137 @@ const initialState = {
             ability: {
                 text: 'Each time you successfully roll to use a Hero card\'s effect, DRAW a card.'
             }
+        },
+        9: {
+            cardType: 'monster',
+            key: 'm9',
+            name: 'Bloodwind Banshee',
+            image: 'assets/monsters/bloodwind-banshee.png',
+            requirements: ['hero', 'hero'],
+            lose: {
+                value: 6,
+                text: 'SACRIFICE a Hero card'
+            },
+            win: {
+                value: 9,
+                text: 'SLAY this Monster Card'
+            },
+            ability: {
+                text: 'Each time another player CHALLENGES you, that player must DISCARD a card.'
+            }
+        },
+        10: {
+            cardType: 'monster',
+            key: 'm10',
+            name: 'Cerberus',
+            image: 'assets/monsters/cerberus.png',
+            requirements: ['wizard', 'hero'],
+            lose: {
+                value: 4,
+                text: 'DISCARD 2 cards'
+            },
+            win: {
+                value: 8,
+                text: 'SLAY this Monster Card'
+            },
+            ability: {
+                text: 'Each time you DRAW a Magic Card, you may play it immediately.'
+            }
+        },
+        11: {
+            cardType: 'monster',
+            key: 'm11',
+            name: 'Dark Arachnid',
+            image: 'assets/monsters/dark-arachnid.png',
+            requirements: ['hero', 'hero'],
+            lose: {
+                value: 5,
+                text: 'SACRIFICE a Hero card'
+            },
+            win: {
+                value: 8,
+                text: 'SLAY this Monster Card'
+            },
+            ability: {
+                text: 'Each time another player plays a Modifier card on one of your rolls, +1 to your roll.'
+            }
+        },
+        12: {
+            cardType: 'monster',
+            key: 'm12',
+            name: 'Dracos',
+            image: 'assets/monsters/dracos.png',
+            requirements: ['hero', 'hero'],
+            lose: {
+                value: 5,
+                text: 'SLAY this Monster Card'
+            },
+            win: {
+                value: 8,
+                text: 'SACRIFICE a Hero card'
+            },
+            ability: {
+                text: 'Each time a hero in your Party is destroyed, DRAW a card.'
+            }
+        },
+        13: {
+            cardType: 'monster',
+            key: 'm13',
+            name: 'Hydra Tribus',
+            image: 'assets/monsters/hydra-tribus.png',
+            requirements: ['bard', 'hero'],
+            lose: {
+                value: 4,
+                text: 'DISCARD 2 cards'
+            },
+            win: {
+                value: 8,
+                text: 'SLAY this Monster Card & DRAW 2 cards'
+            },
+            ability: {
+                text: 'Each time you roll for a Hero card\'s effect, +1 to your roll.'
+            }
+        },
+        14: {
+            cardType: 'monster',
+            key: 'm14',
+            name: 'Rex Major',
+            image: 'assets/monsters/rex-major.png',
+            requirements: ['guardian', 'hero'],
+            lose: {
+                value: 4,
+                text: 'DISCARD 2 cards'
+            },
+            win: {
+                value: 8,
+                text: 'SLAY this Monster Card'
+            },
+            ability: {
+                text: 'Each time a you DRAW a Modifier card, you may reveal it and DRAW a second card.'
+            }
+        },
+        15: {
+            cardType: 'monster',
+            key: 'm15',
+            name: 'Terratuga',
+            image: 'assets/monsters/terratuga.png',
+            requirements: ['hero'],
+            lose: {
+                value: 7,
+                text: 'SACRIFICE a Hero Card'
+            },
+            win: {
+                value: 11,
+                text: 'SLAY this Monster Card'
+            },
+            ability: {
+                text: 'Your Hero cards cannot be destroyed.'
+            }
         }
     },
     availableIds: [],
     slayedIds: [],
-    remainingIds: [1, 2, 3, 4, 5, 6, 7, 8]
+    remainingIds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 }
 
 export default function (state = initialState, action) {
